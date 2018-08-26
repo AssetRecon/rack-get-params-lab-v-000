f4a0adc518148d261e50eb20534212690b1aaa56
Edsel Caprice <caprice@mail.usf.edu>
2018-08-26 16:02:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart == []
-        resp.write "Your car is empty"
+        resp.write "Your cart is empty"
       else
         @@cart.each do |item|
           resp.write "#{item}\n"
